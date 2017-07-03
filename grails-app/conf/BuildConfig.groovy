f397134f9d3f3aff8c93fc4fda3ad9d3141a6133
Pedro Garcia <garcia.ph@gmail.com>
2017-07-03 19:12:00 -0300

    Suporte a MaterializeCSS

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,7 +5,6 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-grails.server.port.http = 5432
 
 grails.project.fork = [
     // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
